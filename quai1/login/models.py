a1fb5226b1eaa18dce436ca1f3c0b0767cd5a34b
Patrick Hervieux <4703153-pherjung@users.noreply.gitlab.com>
2022-02-08 13:39:51 +0100

    Add a list of French speaking depot

--- quai1/login/models.py
+++ quai1/login/models.py
@@ -5,9 +5,26 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     pass
+    DEPOTS = (
+        ('GENEVE', 'Genève'),
+        ('LAUSANNE', 'Lausanne'),
+        ('VALLORBE', 'Vallorbe'),
+        ('BIEL/BIENNE', 'Biel/Bienne'),
+        ('CHAUX-DE-FONDS', 'Chaux-de-Fonds'),
+        ('DELEMONT', 'Délémont'),
+        ('FRIBOURG', 'Fribourg'),
+        ('NEUCHATEL', 'Neuchâtel'),
+        ('PAYERNE', 'Payerne'),
+        ('FRIBOURG', 'Fribourg'),
+        ('ST-MAURICE', 'St-Maurice'),
+        ('VEVEY', 'Vevey')
+
+    )
     url = models.URLField('URL du calendrier SOPREweb',
                           max_length=150)
-    depot = models.CharField(max_length=50)
+    depot = models.CharField('Dépôt',
+                             max_length=14,
+                             choices=DEPOTS)
     phoneNbRegex = RegexValidator(regex=r"^\+?1?[\s|\d]{8,25}$")
     phone_nb = models.CharField('Phone number',
                                 validators=[phoneNbRegex],
