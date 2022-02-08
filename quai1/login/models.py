85f2eb370b6fefcd565383f0f4631f8083c13c37
Patrick Hervieux <4703153-pherjung@users.noreply.gitlab.com>
2022-02-08 13:12:25 +0100

    Add description to some fields

--- quai1/login/models.py
+++ quai1/login/models.py
@@ -5,10 +5,12 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     pass
-    url = models.URLField(max_length=150)
+    url = models.URLField('URL du calendrier SOPREweb',
+                          max_length=150)
     depot = models.CharField(max_length=50)
     phoneNbRegex = RegexValidator(regex=r"^\+?1?[\s|\d]{8,25}$")
-    phone_nb = models.CharField(validators=[phoneNbRegex],
+    phone_nb = models.CharField('Phone number',
+                                validators=[phoneNbRegex],
                                 max_length=16,
                                 unique=True)
 
