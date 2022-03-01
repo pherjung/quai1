e58786b31fd5dffb476eae716f791b5c2d041b42
Patrick Hervieux <4703153-pherjung@users.noreply.gitlab.com>
2022-03-01 17:42:38 +0100

    Move javascript to a file

---
+++ quai1/calendrier/static/calendrier/scripts.js
@@ -0,0 +1,38 @@
+function myFunction(id) {
+  var x = document.getElementById(id);
+  console.log(id)
+  if (x.className === id) {
+    x.className += " responsive";
+
+
+  } else {
+    x.className = id;
+
+
+  }
+
+
+}
+
+function switchBlock() {
+  myFunction('calendar')
+  myFunction('box')
+
+
+}
+
+function displayBlock(obj, id) {
+  var x = document.getElementById('calendar')
+  var y = document.getElementById('box')
+  if (x.className === 'calendar') {
+    x.className += " responsive"
+    y.className += " responsive"
+
+
+  }
+
+  full_date = obj.className.replace(/_/g, ' ')
+  document.getElementById("full_date").innerHTML = full_date
+
+
+}
