1eb5a9d94a057822d42cf9bacf584194bb6f59ce
Patrick Hervieux <4703153-pherjung@users.noreply.gitlab.com>
2022-03-01 21:55:01 +0100

    Do not open dialog on holidays

--- quai1/calendrier/static/calendrier/scripts.js
+++ quai1/calendrier/static/calendrier/scripts.js
@@ -17,17 +17,23 @@ function switchBlock() {
 }
 
 function displayBlock(obj, id) {
+  var shift = obj.querySelector("#shift").className
   var x = document.getElementById('calendar')
   var y = document.getElementById('box')
-  if (x.className === 'calendar') {
+  if (x.className === 'calendar' && shift != 'F') {
     x.className += " responsive"
     y.className += " responsive"
 
+  }
+
+  if (shift != 'F') {
+    full_date = obj.className.replace(/_/g, ' ')
+    document.getElementById("full_date").innerHTML = full_date
 
   }
 
-  full_date = obj.className.replace(/_/g, ' ')
-  document.getElementById("full_date").innerHTML = full_date
+  }
+
 
 
 }
