4dfddfc515df24a7ba8ae3f43d012ac3ee863718
Patrick Hervieux <4703153-pherjung@users.noreply.gitlab.com>
2022-03-02 01:38:28 +0100

    Move URL base to project's URLs

--- quai1/exchange/urls.py
+++ quai1/exchange/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('calendar/validate/', views.save_rest, name='form')
+    path('validate/', views.save_rest, name='form'),
     path('validate_shift/', views.ask_rest, name='ask_rest'),
 ]
