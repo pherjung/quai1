9a0044478b24f61cf27066453d1ba6df376c3760
Patrick Hervieux <4703153-pherjung@users.noreply.gitlab.com>
2022-03-23 12:13:06 +0100

    Manage table Request_shift from admin page

--- quai1/exchange/admin.py
+++ quai1/exchange/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Give_leave, Ask_leave
+from .models import Give_leave, Ask_leave, Request_shift
 
 
 admin.site.register(Give_leave)
 admin.site.register(Ask_leave)
+admin.site.register(Request_shift)
