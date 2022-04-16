a2c591fb5785904f0b8d4642c8155b0faa3e4317
Patrick Hervieux <4703153-pherjung@users.noreply.gitlab.com>
2022-04-17 00:07:17 +0200

    Refactor to more processes

---
+++ quai1/exchange/update_shift.py
@@ -0,0 +1,131 @@
+from datetime import datetime
+from django.db.models import Q
+
+from calendrier.models import Shift
+from .models import Give_leave, Request_leave, Request_shift
+
+
+def search_wishes(user, form_date, request_data, user_note):
+    # Check if there is already requested leave
+    wishes = Request_leave.objects.filter(
+        user_shift__owner__username=user,
+        user_shift__date=form_date,
+    )
+    if not wishes:
+        # Recover all given leaves
+        leaves = Give_leave.objects.filter(
+            shift_id__date=form_date
+        ).exclude(
+            shift_id__owner_id__username=user
+        ).values_list('shift_id')
+        # Recover column ID of all leaves not owned by requester
+        give = Shift.objects.filter(
+            date=form_date,
+            start_hour=None,
+            shift_name__iregex=r'(C|R)T*'
+        ).exclude(owner=user)
+        # Save shifts
+        give_it = 0
+        gift = False  # Useful in case len(give) is 0
+        while give_it < len(give):
+            for index in enumerate(leaves):
+                gift = bool(give[give_it].id in leaves[index[0]])
+
+            save_requested = Request_leave.objects.create(
+                user_shift=request_data,
+                giver_shift=give[give_it],
+                note=user_note,
+                gift=gift
+            )
+            save_requested.save()
+            give_it += 1
+    else:
+        print('There is already an requested leave')
+
+
+def search_shifts(form, form_date, switch):
+    if switch:
+        start_hour1 = form.cleaned_data['start_hour_1']
+        start_hour2 = form.cleaned_data['start_hour_2']
+        end_hour1 = form.cleaned_data['end_hour_1']
+        end_hour2 = form.cleaned_data['end_hour_2']
+        tolerance_end = form.cleaned_data['tolerance_end']
+        tolerance_start = form.cleaned_data['tolerance_start']
+        query = {'date': form_date}
+    else:
+        # To-do
+        print('To-Do')
+
+    condition = 0
+    condition += 1 if start_hour1 else 0
+    condition += 1 if start_hour2 else 0
+    condition += 3 if end_hour1 else 0
+    condition += 3 if end_hour2 else 0
+    match condition:
+        # Search starting from start_minus
+        case 1:
+            start = start_hour1 if start_hour1 else start_hour2
+            minus = (datetime.combine(
+                form_date,
+                start) - tolerance_start).time()
+            if switch:
+                query['start_hour__gt'] = minus
+            else:
+                # To-do
+                print('To-Do')
+        # Search between start_minus and start_plus
+        case 2:
+            minus = (datetime.combine(
+                form_date,
+                start_hour1) - tolerance_start).time()
+            plus = (datetime.combine(
+                form_date,
+                start_hour2) + tolerance_start).time()
+            if switch:
+                query['start_hour__range'] = [minus, plus]
+            else:
+                # To-do
+                print('To-Do')
+        # Search starting from end_minus
+        # Search between requested time
+        case 3:
+            end = end_hour1 if end_hour1 else end_hour2
+            plus = (datetime.combine(
+                form_date,
+                end) + tolerance_end).time()
+            if switch:
+                query['end_hour__lt'] = plus
+            else:
+                # To-do
+                print('To-Do')
+        # Search between end_minus and end_plus
+        case 6:
+            minus = (datetime.combine(
+                form_date,
+                end_hour1) - tolerance_end).time()
+            plus = (datetime.combine(
+                form_date,
+                end_hour2) + tolerance_end).time()
+            if switch:
+                query['end_hour__range'] = [minus, plus]
+            else:
+                # To-do
+                print('To-Do')
+        case 4:
+            start = start_hour1 if start_hour1 else start_hour2
+            minus = (datetime.combine(
+                form_date,
+                start) - tolerance_start).time()
+            end = end_hour1 if end_hour1 else end_hour2
+            plus = (datetime.combine(
+                form_date,
+                end) + tolerance_end).time()
+            if switch:
+                query['start_hour__gt'] = minus
+                query['end_hour__lt'] = plus
+            else:
+                print('to-do')
+        case _:
+            # To-do
+            print('catch false case')
+    return query
--- quai1/exchange/views.py
+++ quai1/exchange/views.py
@@ -7,6 +7,7 @@ from django.db.models import Q
 from calendrier.models import Shift
 from .forms import LeaveForms, RequestLeaveForms
 from .models import Give_leave, Request_leave, Request_shift, Request_log
+from .update_shift import search_wishes, search_shifts
 
 
 def save_leave(request):
@@ -32,141 +33,47 @@ def save_leave(request):
 def request_leave(request):
     if request.method == 'POST':
         form = RequestLeaveForms(request.POST)
-        user = request.user
-
         if form.is_valid():
             requested_date = form.cleaned_data['date']
-            user_note = form.cleaned_data['note']
             form_date = datetime.strptime(requested_date, "%A %d %B %Y")
-            request_data = Shift.objects.get(date=form_date, owner=user)
+            request_data = Shift.objects.get(date=form_date,
+                                             owner=request.user)
+            user_note = form.cleaned_data['note']
             if form.cleaned_data['request_leave'] == 'request_leave':
-                # Check if there is already requested leave
-                wishes = Request_leave.objects.filter(
-                    user_shift__owner__username=user,
-                    user_shift__date=form_date,
-                )
-                if not wishes:
-                    # Recover all given leaves
-                    leaves = Give_leave.objects.filter(
-                        shift_id__date=form_date
-                    ).exclude(
-                        shift_id__owner_id__username=user
-                    ).values_list('shift_id')
-                    # Recover column ID of all leaves not owned by requester
-                    give = Shift.objects.filter(
-                        date=form_date,
-                        start_hour=None,
-                        shift_name__iregex=r'(C|R)T*'
-                    ).exclude(owner=user)
-                    # Save shifts
-                    give_it = 0
-                    gift = False  # Useful in case len(give) is 0
-                    while give_it < len(give):
-                        for index in enumerate(leaves):
-                            gift = bool(give[give_it].id in leaves[index[0]])
-
-                        save_requested = Request_leave.objects.create(
-                            user_shift=request_data,
-                            giver_shift=give[give_it],
-                            note=user_note,
-                            gift=gift
-                        )
-                        save_requested.save()
-                        give_it += 1
-                else:
-                    print('There is already an requested leave')
-
-            if form.cleaned_data['request_leave'] == 'schedule':
-                condition = 0
-                start_hour1 = form.cleaned_data['start_hour_1']
-                condition += 1 if start_hour1 else 0
-                start_hour2 = form.cleaned_data['start_hour_2']
-                condition += 1 if start_hour2 else 0
-                # End stuff
-                end_hour1 = form.cleaned_data['end_hour_1']
-                condition += 3 if end_hour1 else 0
-                end_hour2 = form.cleaned_data['end_hour_2']
-                condition += 3 if end_hour2 else 0
-                tolerance_end = form.cleaned_data['tolerance_end']
-                # default value should be set to tolerance_start
-                tolerance_start = form.cleaned_data['tolerance_start']
-                query = {'date': form_date}
-                match condition:
-                    # Search starting from start_minus
-                    case 1:
-                        start = start_hour1 if start_hour1 else start_hour2
-                        start_minus = (datetime.combine(
-                            form_date,
-                            start) - tolerance_start).time()
-                        query['start_hour__gt'] = start_minus
-                    # Search between start_minus and start_plus
-                    case 2:
-                        start_minus = (datetime.combine(
-                            form_date,
-                            start_hour1) - tolerance_start).time()
-                        start_plus = (datetime.combine(
-                            form_date,
-                            start_hour2) + tolerance_start).time()
-                        query['start_hour__range'] = [start_minus, start_plus]
-                    # Search starting from end_minus
-                    # Search between requested time
-                    case 3:
-                        end = end_hour1 if end_hour1 else end_hour2
-                        end_plus = (datetime.combine(
-                            form_date,
-                            end) + tolerance_end).time()
-                        query['end_hour__lt'] = end_plus
-                    # Search between end_minus and end_plus
-                    case 6:
-                        end_minus = (datetime.combine(
-                            form_date,
-                            end_hour1) - tolerance_end).time()
-                        end_plus = (datetime.combine(
-                            form_date,
-                            end_hour2) + tolerance_end).time()
-                        query['end_hour__range'] = [end_minus, end_plus]
-                    case 4:
-                        start = start_hour1 if start_hour1 else start_hour2
-                        start_minus = (datetime.combine(
-                            form_date,
-                            start) - tolerance_start).time()
-                        query['start_hour__gt'] = start_minus
-                        end = end_hour1 if end_hour1 else end_hour2
-                        end_plus = (datetime.combine(
-                            form_date,
-                            end) + tolerance_end).time()
-                        query['end_hour__lt'] = end_plus
-                    case _:
-                        # To-do
-                        print('catch false case')
-
+                search_wishes(request.user,
+                              form_date,
+                              request_data,
+                              user_note)
+            elif form.cleaned_data['request_leave'] == 'schedule':
+                query = search_shifts(form,
+                                      form_date,
+                                      True)
                 # Shift starting from range start_hour_1 +/- tolerance
                 shifts = Shift.objects.filter(
                     Q(**query)
                     | Q(date=form_date,
                         shift_name__iregex=(r'^200'))
-                ).exclude(owner=user)
+                ).exclude(owner=request.user)
                 log = Request_log.objects.create(
                     user=request.user,
                     date=form_date,
-                    start_hour1=start_hour1,
-                    start_hour2=start_hour2,
-                    tolerance_start=tolerance_start,
-                    end_hour1=end_hour1,
-                    end_hour2=end_hour2,
-                    tolerance_end=tolerance_end,
+                    start_hour1=form.cleaned_data['start_hour_1'],
+                    start_hour2=form.cleaned_data['start_hour_2'],
+                    tolerance_start=form.cleaned_data['tolerance_start'],
+                    end_hour1=form.cleaned_data['end_hour_1'],
+                    end_hour2=form.cleaned_data['end_hour_2'],
+                    tolerance_end=form.cleaned_data['tolerance_end'],
                 )
                 log.save()
 
                 shift_it = 0
                 while shift_it < len(shifts):
-                    save_modify = Request_shift.objects.create(
+                    Request_shift.objects.create(
                         user_shift=request_data,
                         giver_shift=shifts[shift_it],
                         note=user_note,
                         request=log,
-                    )
-                    save_modify.save()
+                    ).save()
                     shift_it += 1
 
     return HttpResponseRedirect(reverse('calendar'))
