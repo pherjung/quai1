6148b67e58f132079a8f261848f97e724a679439
Patrick Hervieux <4703153-pherjung@users.noreply.gitlab.com>
2022-06-02 01:13:27 +0200

    Simplify forms used on admin page

--- src/login/admin.py
+++ src/login/admin.py
@@ -1,21 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Depot
 
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['username',
-                    'first_name',
-                    'last_name',
-                    'email',
-                    'depot',
-                    'phone_nb',
-                    'url']
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
+admin.site.register(Depot)
