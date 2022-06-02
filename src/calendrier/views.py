136ac5d107ebad29e565321828be49a5335618f5
Patrick Hervieux <4703153-pherjung@users.noreply.gitlab.com>
2022-06-02 19:21:23 +0200

    Show on calendar user's wishes and swap opportunities

--- src/calendrier/views.py
+++ src/calendrier/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from exchange.forms import LeaveForms, RequestLeaveForms
+from exchange import models as wish
 from .models import Shift
 
 
@@ -15,9 +16,43 @@ def monthly_calendar(request):
     shifts = Shift.objects.filter(owner=request.user,
                                   date__range=[month_days[0][0],
                                                month_days[-1][-1]])
+    # User ask for help
+    asked_leaves = wish.Request_leave_log.objects.filter(
+        user=request.user,
+        date__range=[month_days[0][0],
+                     month_days[-1][-1]],
+        active=True).values_list('id')
+    asked_shifts = wish.Request_shift_log.objects.filter(
+        user=request.user,
+        date__range=[month_days[0][0],
+                     month_days[-1][-1]],
+        active=True).values_list('id')
+    # User can help
+    swap_shifts = wish.Request_shift.objects.filter(
+        giver_shift__owner=request.user,
+        giver_shift__date__range=[month_days[0][0],
+                                  month_days[-1][-1]],
+        accepted=None,
+        confirmed=False,
+    )
+    swap_leaves = wish.Request_leave.objects.filter(
+        giver_shift__owner=request.user,
+        giver_shift__date__range=[month_days[0][0],
+                                  month_days[-1][-1]],
+        accepted=None,
+        validated=False,
+    )
     for week in month_days:
         week_events = []
         for day in week:
+            # User ask for help
+            div_leave = bool(asked_leaves.filter(date=day))
+            div_shift = bool(asked_shifts.filter(date=day))
+            top_div = 'wish' if div_leave or div_shift else 'default'
+            # User can help
+            swap_shift = bool(swap_shifts.filter(request__date=day))
+            swap_leave = bool(swap_leaves.filter(request__date=day))
+            bottom_div = 'swap' if swap_shift or swap_leave else 'default-swap'
             try:
                 shift = shifts.get(date=day)
                 if shift.date.month == datum.month:
@@ -26,6 +61,7 @@ def monthly_calendar(request):
                     week_events += [[None, None]]
             except Shift.DoesNotExist:
                 week_events += [[None, None]]
+            week_events[-1].extend([top_div, bottom_div])
 
         events.append(week_events)
     context = {'events': events,
